6574ba4e083ff6907f15f05166e9f4e5b973042c
Wojciech Zawadzki <zawadzki1993@hotmail.com>
2024-04-02 20:23:14 +0200

    Add project files.

---
+++ Cwiczenia.4PetlaConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+	static void Main()
+	{
+		Console.Write("Podaj liczbę całkowitą: ");
+		int n = int.Parse(Console.ReadLine());
+
+		int number = 1;
+		for (int i = 1; i <= n; i++)
+		{
+			for (int j = 1; j <= i; j++)
+			{
+				Console.Write($"{number} ");
+				number++;
+			}
+			Console.WriteLine();
+		}
+	}
+}
